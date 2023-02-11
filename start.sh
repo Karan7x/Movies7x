90eabc3b87c91d73daee2609aefecc382202534e
Aditya Choudhary <109758988+AadiX11@users.noreply.github.com>
2023-02-11 21:03:05 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Karan7x/Movies7x /Movies7x
+  git clone https://github.com/Karan7x/Movies7x.git /Movies7x
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Movies7x
