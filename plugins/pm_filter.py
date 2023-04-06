fa8af9e672f05b1a52531774bcbd2be47e2c85fa
Aditya Choudhary <109758988+AadiX11@users.noreply.github.com>
2023-04-06 09:49:19 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -31,8 +31,8 @@ BUTTONS = {}
 SPELL_CHECK = {}
 FILTER_MODE = {}
 
-@Client.on_message(filters.text & filters.incoming)
-async def give_filter(client, message)
+@Client.on_message(filters.group & filters.text & filters.incoming)
+async def give_filter(client, message):
     k = await manual_filters(client, message)
     if k == False:
         await auto_filter(client, message)
